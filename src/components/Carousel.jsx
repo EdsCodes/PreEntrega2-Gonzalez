15049b1a502e042dcfa4dca4481ce1fbf6bfa81f
edscodes <edscodes@gmail.com>
2024-05-30 23:11:11 -0500

    se añade NavLinks a las imagenes del carousel de prods del inicio

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,10 +1,11 @@
-import CelularesImage from '../assets/images/Celulares.png'
-import TabletsPcsImage from '../assets/images/Tablets-pcs.png'
-import AudifonosImage from '../assets/images/Audifonos.png'
-import SmartwatchesImage from '../assets/images/Smartwatches.png'
-import AccesoriosImage from '../assets/images/Otros_Accesorios.png'
-import BannerImgs from './BannerImgs'
-
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import CelularesImage from '../assets/images/Celulares.png';
+import TabletsPcsImage from '../assets/images/Tablets-pcs.png';
+import AudifonosImage from '../assets/images/Audifonos.png';
+import SmartwatchesImage from '../assets/images/Smartwatches.png';
+import AccesoriosImage from '../assets/images/Otros_Accesorios.png';
+import BannerImgs from './BannerImgs';
 
 const Carousel = () => {
     return (
@@ -20,35 +21,45 @@ const Carousel = () => {
                             <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="4" aria-label="Slide 5"></button>
                         </div>
                         <div className="carousel-inner">
-                            <div className="carousel-item active">
-                                <img src={CelularesImage} className="d-block mx-auto w-100" alt="Celulares"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>Celulares</h5>
-                                </div>
+                            <div className="carousel-item imgCarousel1 active">
+                                <NavLink to={"/categoria/celulares"}>
+                                    <img src={CelularesImage} className="d-block mx-auto w-100" alt="Celulares"/>
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>Celulares</h5>
+                                    </div>
+                                </NavLink>
                             </div>
                             <div className="carousel-item">
-                                <img src={TabletsPcsImage} className="d-block mx-auto w-100" alt="Tablets & pcs"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>Tablets & Pc's</h5>
-                                </div>
+                                <NavLink to={"/categoria/tablets & Pcs"}>
+                                    <img src={TabletsPcsImage} className="d-block mx-auto w-100" alt="Tablets & pcs"/>
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>Tablets & Pc's</h5>
+                                    </div>
+                                </NavLink>
                             </div>
                             <div className="carousel-item">
-                                <img src={AudifonosImage} className="d-block mx-auto w-100" alt="Audifonos"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>Audifonos</h5>
-                                </div>
+                                <NavLink to={"/categoria/audifonos"}>
+                                    <img src={AudifonosImage} className="d-block mx-auto w-100" alt="Audifonos"/>
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>Audifonos</h5>
+                                    </div>
+                                </NavLink>
                             </div>
                             <div className="carousel-item">
-                                <img src={SmartwatchesImage} className="d-block mx-auto w-100" alt="Smartwatches"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>Smartwatches</h5>
-                                </div>
+                                <NavLink to={"/categoria/smartwatches"}>
+                                    <img src={SmartwatchesImage} className="d-block mx-auto w-100" alt="Smartwatches"/>
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>Smartwatches</h5>
+                                    </div>
+                                </NavLink>
                             </div>
                             <div className="carousel-item">
-                                <img src={AccesoriosImage} className="d-block mx-auto w-100" alt="Otros Accesorios"/>
-                                <div className="carousel-caption d-none d-md-block">
-                                    <h5>Otros gadgets y accesorios</h5>
-                                </div>
+                                <NavLink to={"/categoria/otros gadgets y accesorios"}>
+                                    <img src={AccesoriosImage} className="d-block mx-auto w-100" alt="Otros Accesorios"/>
+                                    <div className="carousel-caption d-none d-md-block">
+                                        <h5>Otros gadgets y accesorios</h5>
+                                    </div>
+                                </NavLink>
                             </div>
                         </div>
                         <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
@@ -62,11 +73,13 @@ const Carousel = () => {
                     </div>
                 </div>
             </div>
+            <div className="row">
                 <div className="col">
                     <BannerImgs />
                 </div>
+            </div>
         </div>
-    )
+    );
 }
 
 export default Carousel;
