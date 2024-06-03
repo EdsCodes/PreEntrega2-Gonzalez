4bf8e539d1367a7902f29d74226e05882fe334b9
edscodes <edscodes@gmail.com>
2024-06-02 20:38:45 -0500

    se añade funcionalidad de esconder el carrito en caso de no haber productos en el mismo

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -17,14 +17,9 @@ const CartWidget = () => {
             </Link>
         </button>
         );
-    }
+    } else 
     return (
-        <button type="button" className="btn btn-light text-dark botonCarrito">
-        <Link className="nav-link position-relative" to="/Cart">
-            <img src={Cart3} alt="Imagen carrito" />
-            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"></span>
-        </Link>
-    </button>
+        null
     );
 };
 
